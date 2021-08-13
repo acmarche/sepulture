32770381e1bf58019916c463f550b0f4c649cf64
jfsenechal <jfsenechal@gmail.com>
2021-08-13 22:48:10 +0200

    gallery controller

---
+++ assets/gallery.js
@@ -0,0 +1,17 @@
+import lightGallery from 'lightgallery';
+// Plugins
+import lgThumbnail from 'lightgallery/plugins/thumbnail'
+import lgZoom from 'lightgallery/plugins/zoom'
+
+/* j'arrive pas a lappeler depuis twig :-( */
+
+//window.addEventListener("load", () => {
+
+//});
+export default function () {
+    console.log('zeze');
+    lightGallery(document.getElementById('lightgallery'), {
+        plugins: [lgZoom, lgThumbnail],
+        speed: 500,
+    });
+};
