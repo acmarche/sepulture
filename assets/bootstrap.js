430a9a4258e8791dd66ad5352f4536a35c73d0d8
jfsenechal <jfsenechal@gmail.com>
2022-03-23 12:06:05 +0100

    CheckboxSelectAll

--- assets/bootstrap.js
+++ assets/bootstrap.js
@@ -1,4 +1,5 @@
 import { startStimulusApp } from '@symfony/stimulus-bridge';
+import CheckboxSelectAll from "stimulus-checkbox-select-all"
 
 // Registers Stimulus controllers from controllers.json and in the controllers/ directory
 export const app = startStimulusApp(require.context(
@@ -9,3 +10,5 @@ export const app = startStimulusApp(require.context(
 
 // register any custom, 3rd party controllers here
 // app.register('some_controller_name', SomeImportedController);
+
+app.register('checkbox-select-all', CheckboxSelectAll);
--- assets/controllers/dropzone_controller.js
+++ assets/controllers/dropzone_controller.js
@@ -1,4 +1,4 @@
-import {Controller} from "stimulus";
+import {Controller} from "@hotwired/stimulus";
 import Dropzone from "dropzone";
 
 export default class extends Controller {
