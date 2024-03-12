ec4eec7a5f5e5facd0bcc4d88ee0995a92ba44f7
jfsenechal <jfsenechal@gmail.com>
2024-03-12 19:35:51 +0100

    up security

--- config/packages/security.php
+++ config/packages/security.php
@@ -2,9 +2,16 @@
 
 use AcMarche\Sepulture\Entity\User;
 use AcMarche\Sepulture\Security\AppAuthenticator;
-use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symfony\Config\SecurityConfig;
 
-return static function (ContainerConfigurator $containerConfigurator): void {
+return static function (SecurityConfig $security) {
+
+    $security->provider('sepulture_user_provider')
+        ->entity()
+        ->class(User::class)
+        ->property('email');
+
+    // @see Symfony\Config\Security\FirewallConfig
     $main = [
         'provider' => 'sepulture_user_provider',
         'logout' => [
@@ -12,44 +19,20 @@ return static function (ContainerConfigurator $containerConfigurator): void {
         ],
         'form_login' => [],
         'entry_point' => AppAuthenticator::class,
-        'switch_user' => true,
-        'custom_authenticator' => AppAuthenticator::class,
         'login_throttling' => [
-            'max_attempts' => 6, //per minute...
+            'max_attempts' => 6, // per minute...
+        ],
+        'remember_me' => [
+            'secret' => '%kernel.secret%',
+            'lifetime' => 604800,
+            'path' => '/',
+            'always_remember_me' => true,
         ],
     ];
 
-    // focant en fin de
-    /* @see PasswordHasherFactory.php */
-    // $config['encode_as_base64'] = false;
-    // $config['iterations'] = 1;
-    $containerConfigurator->extension('security', [
-        'password_hashers' => [
-            'legacy' => [
-                'algorithm' => 'sha512',
-                'encode_as_base64' => false,
-                'iterations' => 13,
-            ],
-            'AcMarche\Sepulture\Entity\Security\User' => [
-                'algorithm' => 'auto',
-                'migrate_from' => [
-                    'legacy',
-                ],
-            ],
-        ],
-        'providers' => [
-            'sepulture_user_provider' => [
-                'entity' => [
-                    'class' => User::class,
-                    'property' => 'email',
-                ],
-            ],
-        ],
-        'firewalls' => [
-            'main' => $main,
-        ],
-        'role_hierarchy' => [
-            'ROLE_SEPULTURE_ADMIN' => ['ROLE_SEPULTURE_EDITEUR'],
-        ],
-    ]);
+    $authenticators = [AppAuthenticator::class];
+
+    $main['custom_authenticators'] = $authenticators;
+    $security->roleHierarchy('ROLE_SEPULTURE_ADMIN', ['ROLE_SEPULTURE_EDITEUR']);
+    $security->firewall('main', $main);
 };
--- src/Security/AppAuthenticator.php
+++ src/Security/AppAuthenticator.php
@@ -10,6 +10,7 @@ use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Http\Authenticator\AbstractLoginFormAuthenticator;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\CsrfTokenBadge;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\RememberMeBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\PasswordCredentials;
 use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
@@ -39,6 +40,7 @@ class AppAuthenticator extends AbstractLoginFormAuthenticator
         $badges =
             [
                 new CsrfTokenBadge('authenticate', $token),
+                new RememberMeBadge()
             ];
 
         return new Passport(
